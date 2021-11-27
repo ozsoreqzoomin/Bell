b186af8d451dfeb3187114130a8b819bc6cf6758
oz.soreq@zoominsoftware.com <oz.soreq@zoominsoftware.com>
2021-11-27 10:00:08 +0000

    Base

--- models/finance/stg_AccountActualARRMonthlySnapshot.sql
+++ models/finance/stg_AccountActualARRMonthlySnapshot.sql
@@ -1,4 +1,16 @@
-with Account_ARR_MonthlySnapshots as (        
+with Opportunity as (
+    select * from {{ ref('Fact_Opportunities')}}
+),
+
+Dates as (
+    select * from {{ ref('Dim_Dates')}}
+),
+
+Accounts as (
+    select * from {{ ref('Dim_Accounts')}}
+),
+
+Account_ARR_MonthlySnapshots as (        
  
 select Date, account_id, Sum(Total_ARR_C) Current_ARR,
 Case when LEAD(Sum(Total_ARR_C)) over (Partition by account_id order by Date desc) is NULL then Sum(Total_ARR_C) else Sum(Total_ARR_C)-LEAD(Sum(Total_ARR_C)) over (Partition by account_id order by Date desc) end as ARR_Change,
@@ -6,9 +18,9 @@ Case when LEAD(Sum(Total_ARR_C)) over (Partition by account_id order by Date des
 
 from
  (select D.Date,account_id,SUBSCRIPTION_STATUS_C  AccountStatus,SUBSCRIPTION_END_DATE_C  End_Date, SUBSCRIPTION_START_DATE_C  Start_Date, Close_Date, Stage_Name, Total_ARR_C,type 
-  from SALESFORCE2.Opportunity o 
+  from Opportunity o 
   left join
-  (select distinct DATE from SALESFORCE2.Dim_Date where DAY(DATE) = 1
+  (select distinct DATE from Dates where DAY(DATE) = 1
     and DATE > '2015-01-01' AND DATE < current_date()) D
     on D.Date < o.SUBSCRIPTION_END_DATE_C AND D.DATE > o.SUBSCRIPTION_START_DATE_C
   where 
@@ -28,5 +40,5 @@ from
   ELSE 'No change' END AS ARR_Change_Type, 
  a.Type, ams.DESCRANK
   from Account_ARR_MonthlySnapshots ams
-  join SALESFORCE2.Account a on a.ID = ams.Account_ID
+  join Accounts a on a.ID = ams.Account_ID
   where Date IS NOT null
