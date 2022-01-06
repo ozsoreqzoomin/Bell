4c52576950d17fd5cf40b75e431f366594722fe3
oz.soreq@zoominsoftware.com <oz.soreq@zoominsoftware.com>
2022-01-06 12:46:38 +0000

    Fixing bases to Airbyte

--- models/2-base/salesforce/base_Accounts.sql
+++ models/2-base/salesforce/base_Accounts.sql
@@ -1,1 +1,15 @@
-select * from {{ source('SALESFORCE2', 'Account') }}
+SELECT 
+    ID as Account_ID,
+    NAME as Account_Name,
+    TYPE as Account_Type,
+    INDUSTRY as Industry,
+    CREATEDDATE as Account_Creation_Date,
+    TIMEZONE__C as Account_Timezone,
+    TERRITORY__C as Account_Territory,
+    ACTIVE_ARR__C as Active_ARR,
+    ACTUAL_ICU__C as Actual_ICU,
+    CHURN_RISK__C as Churn_Risk,
+    ACCOUNT_CSM__C as Account_CSM,
+    ACCOUNT_MANAGER__C as Account_Manager
+FROM
+    {{source('SALESFORCE', 'src_Account')}}
