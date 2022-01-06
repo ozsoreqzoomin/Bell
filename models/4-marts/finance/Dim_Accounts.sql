878f7313232d66e29eeb80735395ea5cf9fb6818
oz.soreq@zoominsoftware.com <oz.soreq@zoominsoftware.com>
2022-01-06 15:26:44 +0000

    Migration to Airbyte Done

--- models/4-marts/finance/Dim_Accounts.sql
+++ models/4-marts/finance/Dim_Accounts.sql
@@ -1,5 +1,20 @@
-select a.*, arr.SNAPSHOT_DATE as FirstSubscriptionDate from {{ ref('base_Accounts')}} a
- left join
- {{ ref('stg_MonthlyARR')}} arr
-  on
-  a.ID = arr.account_id AND arr.ARR_Change_Type = 'New'
+SELECT
+    a.Account_ID,
+    a.Account_Name,
+    a.Account_Type,
+    a.Industry,
+    a.Account_Creation_Date,
+    a.Account_Timezone,
+    a.Account_Territory,
+    a.Active_ARR,
+    a.Actual_ICU,
+    a.Churn_Risk,
+    a.Account_CSM,
+    a.Account_Manager,
+    arr.SNAPSHOT_DATE AS FirstSubscriptionDate
+ FROM
+    {{ref('base_Accounts')}} a
+ LEFT JOIN
+ {{ref('stg_MonthlyARR')}} arr
+ ON
+  a.Account_ID = arr.Account_ID AND arr.ARR_Change_Type = 'New'
